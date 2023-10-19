a88e8983e83538b69b43d1c329d07880c46a2c70
soheeyeo <sohee407@gmail.com>
2023-10-19 12:11:49 +0900

    Edit Dropdown component

--- app/_components/Dropdown.tsx
+++ app/_components/Dropdown.tsx
@@ -53,65 +53,84 @@ const Dropdown = ({ language = "markdown", onChange }: DropdownProps) => {
     };
 
     return (
-        <>
-            <Listbox value={selected} onChange={handleChange}>
-                <div className="relative mt-1">
-                    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                        <span className="block truncate">{selected}</span>
-                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                            <ChevronUpDownIcon
-                                className="h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                            />
-                        </span>
-                    </Listbox.Button>
-                    <Transition
-                        as={Fragment}
-                        leave="transition ease-in duration-100"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                            {languages.map((language, index) => (
-                                <Listbox.Option
-                                    key={index}
-                                    className={({ active }) =>
-                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                            active
-                                                ? "bg-amber-100 text-amber-900"
-                                                : "text-gray-900"
-                                        }`
-                                    }
-                                    value={language}
-                                >
-                                    {({ selected }) => (
-                                        <>
-                                            <span
-                                                className={`block truncate ${
-                                                    selected
-                                                        ? "font-medium"
-                                                        : "font-normal"
-                                                }`}
-                                            >
-                                                {language}
-                                            </span>
-                                            {selected ? (
-                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                                                    <CheckIcon
-                                                        className="h-5 w-5"
-                                                        aria-hidden="true"
-                                                    />
-                                                </span>
-                                            ) : null}
-                                        </>
-                                    )}
-                                </Listbox.Option>
-                            ))}
-                        </Listbox.Options>
-                    </Transition>
-                </div>
-            </Listbox>
-        </>
+        <Listbox value={selected} onChange={handleChange}>
+            {({ open }) => (
+                <>
+                    <div className="relative mt-1">
+                        <Listbox.Button className="relative w-full cursor-default rounded-xl border border-gray-300 bg-gray-900 py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+                            <span className="flex items-center">
+                                <span className="ml-3 block">{selected}</span>
+                            </span>
+                            <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                                <ChevronUpDownIcon
+                                    className="h-5 w-5 text-gray-100"
+                                    aria-hidden="true"
+                                />
+                            </span>
+                        </Listbox.Button>
+
+                        <Transition
+                            show={open}
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="absolute z-10 mt-0 max-h-56 w-full overflow-hidden rounded-md bg-gray-900 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm mt-1">
+                                <Listbox.Options className="max-h-56 w-full overflow-auto">
+                                    {languages.map((language, index) => (
+                                        <Listbox.Option
+                                            key={index}
+                                            className={({ active }) =>
+                                                `${
+                                                    active
+                                                        ? "text-white bg-indigo-600"
+                                                        : "text-gray-100"
+                                                }
+                        relative cursor-default select-none py-2 pl-3 pr-9`
+                                            }
+                                            value={language}
+                                        >
+                                            {({ selected, active }) => (
+                                                <>
+                                                    <div className="flex items-center">
+                                                        <span
+                                                            className={`${
+                                                                selected
+                                                                    ? "font-semibold"
+                                                                    : "font-normal"
+                                                            } ml-3 block truncate`}
+                                                        >
+                                                            {language}
+                                                        </span>
+                                                    </div>
+
+                                                    {selected ? (
+                                                        <span
+                                                            className={`${
+                                                                active
+                                                                    ? "text-white"
+                                                                    : "text-indigo-600"
+                                                            }
+                            absolute inset-y-0 right-0 flex items-center pr-4`}
+                                                        >
+                                                            <CheckIcon
+                                                                className="h-5 w-5"
+                                                                aria-hidden="true"
+                                                            />
+                                                        </span>
+                                                    ) : null}
+                                                </>
+                                            )}
+                                        </Listbox.Option>
+                                    ))}
+                                </Listbox.Options>
+                            </div>
+                        </Transition>
+                    </div>
+                </>
+            )}
+        </Listbox>
     );
 };
 
