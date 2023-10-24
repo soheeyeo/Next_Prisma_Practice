61b805fec3fcc6d279159bfd4ead9adccf06c56f
soheeyeo <sohee407@gmail.com>
2023-10-24 12:51:49 +0900

    Create PostType interface

---
+++ types/post.ts
@@ -0,0 +1,10 @@
+export interface PostType {
+    id: number;
+    title: string;
+    code: string;
+    language: string;
+    totalLikes: number;
+    totalComments: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
