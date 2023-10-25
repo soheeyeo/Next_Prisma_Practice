2021659e4f34c76108e75a51397e7f978b1be3c6
soheeyeo <sohee407@gmail.com>
2023-10-25 10:54:11 +0900

    Create PostItem component

---
+++ app/_components/PostItem.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+
+import formatTimeAgo from "@/utils/timeAgo";
+import highlight from "@/utils/highlight";
+import { PostType } from "@/types/post";
+
+interface PostFormProps {
+    href: string;
+    post: PostType;
+}
+
+const PostItem = ({ href, post }: PostFormProps) => {
+    return (
+        <div
+            className={
+                "lex flex-col overflow-hidden rounded-lg shadow-lg my-10"
+            }
+        >
+            <div className="flex flex-1 flex-col justify-between p-6 pb-3">
+                <Link href={href}>
+                    <div className="mt-2 items-center">
+                        <div className="ml-4 flex-1">
+                            <div className="text-right">
+                                <p className="text-sm text-gray-300">
+                                    {formatTimeAgo(post.createdAt)}
+                                </p>
+                            </div>
+                            <div className="flex-1 mt-1">
+                                <p className="text-xl font-semibold text-gray-100">
+                                    {post.title.substring(0, 50)}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <pre className="mt-4 mx-5 max-h-52 overflow-hidden border-b border-gray-700 whitespace-pre-wrap break-words">
+                        {post.language ? (
+                            <code
+                                className={`language-${post.language}`}
+                                dangerouslySetInnerHTML={{
+                                    __html: highlight(post.code, post.language),
+                                }}
+                            ></code>
+                        ) : (
+                            <code>{post.code}</code>
+                        )}
+                    </pre>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
